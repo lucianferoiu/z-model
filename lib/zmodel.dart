a175170618461b34b98ca7f9deef3009acfdf77f
Lucian Feroiu <lucianferoiu@gmail.com>
2022-01-24 09:01:13 +0200

    Stub of the z-model library

---
+++ lib/zmodel.dart
@@ -0,0 +1,4 @@
+library zmodel;
+
+class ZModel {
+}
---
+++ test/zmodel_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:zmodel/zmodel.dart';
+
+void main() {
+  test('dummy', () {
+    final model = ZModel();
+  });
+}

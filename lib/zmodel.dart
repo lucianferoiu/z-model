8abb7d6dca10da4f4fe9d3c9384c115734d09dc4
Lucian Feroiu <lucianferoiu@gmail.com>
2022-02-07 12:05:30 +0200

    model ID and unique roots

--- lib/zmodel.dart
+++ lib/zmodel.dart
@@ -1,4 +1,17 @@
 library zmodel;
 
+typedef ID = String; // later on, we may wish to optimize for memory/speed
+
+Map<ID, ZModel> _roots = {}; // private roots to all the model trees in this (main) isolate
+
 class ZModel {
+  
+  final ID id;
+  ZModel(this.id);
+
+  factory ZModel.root(ID rootId) {
+    return _roots.putIfAbsent(rootId, () => ZModel(rootId));
+  }
+
+
 }
--- test/zmodel_test.dart
+++ test/zmodel_test.dart
@@ -3,7 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:zmodel/zmodel.dart';
 
 void main() {
-  test('dummy', () {
-    final model = ZModel();
+  test('only one model tree with a given root', () {
+    final model1 = ZModel.root("fancy");
+    final model2 = ZModel.root("fancy");
+    expect(model1 == model2, true);
   });
 }
